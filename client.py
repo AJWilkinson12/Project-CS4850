12d5b7cb29f3d4cf2de7bbc427226d788cbde439
AJ Wilkinson <ajwilkinson18@gmail.com>
2022-03-15 11:34:05 -0500

    V1 Completed

--- client.py
+++ client.py
@@ -1,75 +1,90 @@
-#Created by Ben Bradshaw, 3/6/22.
-#This file contains the client code for V2 of my simple chat room client.
+#Created by AJ Wilkinson
+#PawPrint: ASWD62  StudentID: 14307129
 
 import socket
-from threading import Thread
-from time import sleep
+import sys
+import time
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 17129 # The port used by the server
+#######################################################
+###    Creating a socket using Python Socket & Sys  ###
+#######################################################
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+port = 8002
+connections = 0
+loginFlag = 0
+print("")
+while connections == 0:
+    try:
+        sock.connect((host, port))
+        connections = connections + 1
+    except:
+        pass
 
+#####################################################
+#      Formatting for when the client connects      #
+#####################################################
+print("YOU'RE CONNECTED")
+print("")
+print("")
+print("*****COMMANDS AVAILABLE*****")
+print("")
+print("Type 'login [USERNAME] [PASSWORD]' to login")
+print("Type 'newuser [USERNAME] [PASSWORD]' to create a newuser")
+print("           username must be less than 32 characthers")
+print("           password must be between 4 & 8 characters")
+print("Type 'send' to send a message")
+print("Type 'logout' to logout")
+print("")
+print("")
 
-#Thread that constantly listens for incoming data for the server. Prints when received.  If stop_threads is set to true from main loop or is informed that server is at max capacity, stop listenting and return. 
-def listen_for_server(s):
-    global stop_threads
-    while True:
-        data = s.recv(1024).decode()
-        print(data)
-        if(data == "Sorry. Max active users has been reached."):
-            return
-        if stop_threads:
-            return
-       
+## While loop to run the program
+while(True):
+    if (loginFlag == 0): ##check if logged in or not
+        message = input(">> ")
+        sock.send(message.encode()) # python3 defaults to UTF-8 encoding for socket
+        message = sock.recv(1024).decode() # decoding what is received by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    print("My chat room client. Version One \n")
-    logged_in = False
-    stop_threads = False
-    th = Thread(target = listen_for_server,args = [s])
-    th.daemon
-    th.start()
-    while True:
-        send = input()
-        command = send.split(" ")
-        #Login logic - validates input then sends to server.
-        if command[0] == "login":
-            if len(command) != 3:
-                print("Invalid command. Format is login Username Password")
-                continue
-            s.send(send.encode())
-        #Newuser logic - validates input then sends to server
-        elif command[0] == "newuser":
-            if len(command) != 3:
-                print("Invalid command. Format is newuser Username Password")
-                continue
-            if len(command[1]) <3 or len(command[1]) >32:
-                print("Invalid username.  Username must be between 3 and 32 characters.")
-                continue
-            if len(command[2]) < 4 or len(command[2]) >8:
-                print("Invalid password. Password must be between 4 and 8 characters")
-                continue
-            s.send(send.encode())
-         
-        #Send logic - validates message and sends to server.
-        elif command[0] == "send":
-            if len(command) > 1:
-                message = send.split(' ', 2)[2]
-                if len(message) <1 or len(message) > 256:
-                    print("Invalid message.  Messages must be between 1 and 256 characters")
-                    continue
-            s.send(send.encode())
-        elif command[0] == "who" and len(command) == 1:
-            s.send(send.encode())
-       
-        #Logout logic - ensures user is logged in, then sends logout to server, breaks loop and closes connection.
-        elif send == "logout":
-            stop_threads = True
-            s.send(send.encode())
-            sleep(1)
+        ##########################
+        ### checking for login ###
+        ##########################
+        if message.split(" ")[0] == "Server:" and message.split(" ")[2] == "joins":
+            #if message.split(" ")[2] == "joins" #login message would contain these two words
+            newUsername = message.split(" ")[1]
+            loginFlag = 1 # if login good, pump counter to get name next to input
+
+        ##########################
+        ### checking for quit  ###
+        ##########################
+        elif message.split(" ")[0] == "Server:" and "has decided to quit!" in message:
+            print(message)
             break
-        else:
-            print("Command not recognized.")
-            continue
-s.close()
 
+        print(message)
+
+##################################
+### if statement after log in  ###
+# used to add login name to chat #
+##################################
+    if(loginFlag == 1): ##used for when logged in
+        message = input(newUsername+">> ")
+        sock.send(message.encode())
+        message = sock.recv(1024).decode()
+        #########################
+        ## checking for logout ##
+        #########################
+        if message.split(" ")[0] == "Server:" and "left" in message:
+            loginFlag = 0
+        elif message.split(" ")[0] == "Server:" and "has decided to quit!" in message:
+            print(message)
+            break
+        print(message)
+
+##############################
+##### while loops exited #####
+## close socket and program ##
+##############################
+print("")
+print("Please come again!")
+print("")
+sock.close()
--- server.py
+++ server.py
@@ -1,43 +1,40 @@
-#Created by Ben Bradshaw, 3/6/22.
-#This file contains the server code for V2 of my simple chat room client.
+#Created by AJ Wilkinson
+#PawPrint: ASWD62  StudentID: 14307129
 
 import socket
 import re
-from time import sleep
-from threading import Thread
+import sys
+import time
 
-HOST = "127.0.0.1"
-PORT = 17129
-MAXCLIENTS = 3
-active_connections = {}
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = socket.getHOSTname()
+PORT = 8002
+isLoggedIn = 0
+sock.bind((HOST,PORT))
+sock.listen(3)
 
-#Reads exisiting users from users.txt file and returns to chat_control.
+print ("Waiting for connection...")
+
+conn, addr = sock.accept() #accepts connects from anyone
+
+print("")
+print("A client has connected!")
+print("")
+
+
 def read_users():
     with open("users.txt") as f:
         users = f.readlines()
     f.close()
     return users
 
-#Formatting new_user to match existing users and writing to the users.txt file.
-def update_users(c_uid, c_pass):
-    new_user = "("+c_uid+", "+c_pass+")"
-    with open("users.txt", "a") as f:
-        f.write("\n")
-        f.write(new_user)
-    f.close()
+def new_user(users, c_uid, c_pass):
+    if check_match(users, c_uid, c_pass):
+        return False
+    else:
+        update_users(c_uid, c_pass)
+        return True
 
-#Login logic for login command, returns None if no match is found or user is already logged in.
-def login(users, la_uid, la_pass):
-    #Login Logic
-        #
-        if(la_uid in active_connections.keys()):
-            return None
-        if check_match(users, la_uid, la_pass):
-            return la_uid
-        #If no match is found, inform user and return NULL
-        else:
-            return None
-#Checks if current given uid and pass matches existing users in users,txt.  Used in new_user and login.
 def check_match(users, c_uid, c_pass):
     for user in users:
         #Stripping each registered users extra characters
@@ -47,160 +44,86 @@ def check_match(users, c_uid, c_pass):
         #Check if current check variables match.
         if registered_u[0] == c_uid and registered_u[1] == c_pass:
             return True
-        
-#Logic for a New user for new_user command.
-def new_user(users, c_uid, c_pass):
-    if check_match(users, c_uid, c_pass):
-        return False
-    else:
-        update_users(c_uid, c_pass)
-        return True
 
-#Sends message to all active connections.
-def send_all(active_u, message):
-    for uid, conn in active_connections.items():
-        if(uid == active_u):
-            continue
-        conn.send((active_u + ": " + message).encode())
-    return
-        
-#Informs all other active connections that a user has logged out.
-def send_all_logout(active_u, message):
-    for uid, conn in active_connections.items():
-        if(uid == active_u):
-            continue
-        conn.send((active_u + message).encode())
-    return
+def update_users(c_uid, c_pass):
+    new_user = "("+c_uid+", "+c_pass+")"
+    with open("users.txt", "a") as f:
+        f.write("\n")
+        f.write(new_user)
+    f.close()
 
-#Informs all other active connections that a user has logged in.
-def send_all_login(active_u):
-    for uid, conn in active_connections.items():
-        if(uid == active_u):
-            continue
-        conn.send((active_u + " joins.").encode())
-    return
-   
-#Returns a string listing each active connections UID.
-def who():
-    message = ""
-    uids = list(active_connections.keys())
-    for uid in active_connections:
-        if uid == uids[-1]:
-            message += uid
+
+while True:
+    data = conn.recv(1024).decode()
+    commandWord = data.split(" ")[0]
+
+    if(commandWord == "login"):
+        if(isLoggedIn !=0):
+            data = "Server: " + newUsername + " is already logged in!"
         else:
-            message += uid +", "
-    return message
-    
-#Main chat loop for each currently active thread/client.
-def chat_control(conn, addr):
-    logged_in = False
-    active_u = ""
+            try:
+                newUsername = data.split(" ")[1]
+                newPassword = data.split(" ")[2]
+                print("Login Failed.")
+                data = "Login failed. Please check username and password."
+                with open("users.txt", "r") as f:
+                    for line in f:
+                        if newUsername in line:
+                            if newPassword in line:
+                                print(newUsername + " login")
+                                data = "Server: " + newUsername + " joins"
+                                isLoggedIn = 1
+            except:
+                data = "Login failed. Please check username and password."
+            finally:
+                f.close()
+        conn.send(data.encode())
 
-    users = read_users()
-    
+    elif(commandWord == "send"):
+        if(isLoggedIn == 0):
+            data = "Server: Denied. Please Login First"
+        else:
+            data = newUsername + ": " + data[5:]
+            print (newUsername + " " + data[5:])
+        conn.send(data.encode())
 
-    while True:
-        #Listening for a command to come through from the connected client.
-        data = conn.recv(1024).decode()
-        command = data.split(" ")
-        #Login Logic - If user is already logged in, inform them.  If not and credentials are validated and user isn't already an active connection, set the active user and add to active connections.  Inform other currently actice connections.
-        if command[0] == "login":
-            if(logged_in):
-                conn.send("Already logged in.".encode())
-                continue
-            else:
-                active_u = login(users, command[1], command[2])
-                if active_u is not None:
-                    active_connections[active_u] = conn;
-                    logged_in = True
-                    send_all_login(active_u)
-                    print(active_u + " logged in.")
-                    conn.send("login confirmed".encode())
-                    continue
-                else:
-                    conn.send("Denied.  Username or Password incorrect or user is already logged in.".encode())
-                    continue
-            
-        #Newuser Logic - If the user is already logged in, inform them. If not and credentials do not already exist in users.txt, create the new user and add it to users.txt, then inform the user to login.
-        elif command[0] == "newuser":
-            if(logged_in):
-                conn.send("Cannot create new user while logged  in.".encode())
-                continue
-            if new_user(users, command[1], command[2]):
-                print("New user created.")
-                users = read_users()
-                conn.send("New user account created. Please login.".encode())
-                continue
-            else:
-                conn.send("Denied. User account already exists.".encode())
-                continue
-                      
-                            
-        #Send logic - If not logged in, inform the user.  If command is all, send their message to all active connections.  If a UID is specified, send to only that active connection.
-        elif command[0] == "send":
-            if(not logged_in):
-                conn.send("Denied. Must be logged in to use the send command.".encode())
-                continue
-            #Stripping send from command and appending active user to the beginning.
-            if len(command) > 1:
-                if command[1] == "all":
-                    message = data.split(' ', 2)[2]
-                    send_all(active_u, message)
-                    print(active_u + ": " + message)
-                    continue
-                elif command[1] in active_connections:
-                    message = data.split(' ', 2)[2]
-                    active_connections[command[1]].send((active_u + ": " + message).encode())
-                    print(active_u + "(to " + command[1] + "): " + message)
-                    continue
-                else:
-                    conn.send("Error sending message.  Either user wasn't online or all wasn't specified.".encode())
-                    continue
-            else:
-                conn.send("Error on send, a user or all must be specified after 'send' command.".encode())
-                continue
-        elif command[0] == "who":
-            active_us = who()
-            conn.send(active_us.encode())
-                    
-        #Logout Logic - Removes active user, closes connection and breaks back to listenting for a connection from client.  Informs other active connections of user logout.
-        elif command[0] == "logout":
-            global active_threads
-            if(not logged_in):
-                conn.send("Must be logged in to log out!".encode())
-                continue
-            logged_in = False
-            print(active_u + " log out.")
-            send_all_logout(active_u, " left.")
-            conn.send("Successfully logged out.".encode())
-            active_threads -= 1
-            active_connections.pop(active_u)
-            sleep(2)
-            conn.close()
-            active_u = ""
-            return
-            
-#Main for server, listens for new connections up to 3.  When a new connection is found create a Thread whos target is the main chat loop.  if max is met, inform user and prompt them to logout.
-                        
-if __name__ == "__main__":
-    print("My chat room client. Version One\n");
-    active_threads = 0
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        while True:
-            s.listen()
-            conn, addr = s.accept()
-            if active_threads < MAXCLIENTS:
-                active_threads +=1
-                Thread(target = chat_control,args = (conn,addr)).start()
-            else:
-                conn.send("Sorry. Max active users has been reached. Please logout and try again later".encode())
-                sleep(1)
-                conn.close()
-            
-                
-        
+    elif(commandWord == "newuser"):
+        users = read_users()
+        if(isLoggedIn):
+            conn.send("Cannot create new user while logged  in.".encode())
+            continue
+        if new_user(users, data.split(" ")[1], data.split(" ")[2]):
+            print("New user created.")
+            users = read_users()
+            conn.send("New user account created. Please login.".encode())
+            continue
+        else:
+            conn.send("Denied. User account already exists.".encode())
+            continue
 
 
+    elif(commandWord == "logout"):
+        if(isLoggedIn == 0):
+            data = "You're not logged in"
+            conn.send(data.encode())
+        else:
+            print(newUsername + " logout")
+            data = "Server: " + newUsername + " left."
+            conn.send(data.encode())
+            isLoggedIn = isLoggedIn - 1
+
+    else:
+        print("Couldn't read input")
+        data = "Server: couldn't read input"
+        conn.send(data.encode())
+
+    if not data:
+        break
 
 
+print("")
+print("Client has disconnected!")
+conn.close()
+print("")
+print("Exiting server...")
+print("")
